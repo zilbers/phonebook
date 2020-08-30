ccc7272b843e6bde69fcaca999c39db6d6b5b694
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-08-30 15:37:32 +0300

    rest API full
    Made a complete rest API server

--- index.js
+++ index.js
@@ -1,38 +1,92 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-const persons = [
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+let persons = [
   {
-    name: "Arto Hellas",
-    number: "050-1123423",
-    id: "1",
+    name: 'Arto Hellas',
+    number: '050-1123423',
+    id: 1,
   },
   {
-    name: "Mor Food",
-    number: "054-1144423",
-    id: "2",
+    name: 'Mor Food',
+    number: '054-1144423',
+    id: 2,
   },
   {
-    name: "Aym An",
-    number: "052-1222123",
-    id: "3",
+    name: 'Aym An',
+    number: '052-1222123',
+    id: 3,
   },
   {
-    name: "Pea Nutbutter",
-    number: "058-1177763",
-    id: "4",
+    name: 'Pea Nutbutter',
+    number: '058-1177763',
+    id: 4,
   },
   {
-    name: "Coco Loca",
-    number: "050-1126763",
-    id: "5",
+    name: 'Coco Loca',
+    number: '050-1126763',
+    id: 5,
   },
 ];
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   res.send(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((personFromList) => personFromList.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.get('/api/info', (req, res) => {
+  const info = `PhoneBook has info for ${persons.length} people
+
+${new Date()}`;
+  res.send(info);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  if (
+    !body.name ||
+    !body.number ||
+    persons.some((person) => person.name === body.name)
+  ) {
+    return res.status(400).json({
+      error: "Can't update PhoneBook with this data",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  res.json(persons);
 });
 
 const PORT = 3001;
